62b9adb5104f4cb4e67d65550e6be6b225285f90
syannithusan <syannithusan@gmail.com>
2025-09-24 10:59:23 +0530

    24/09 start

--- movieApp/src/components/Card.jsx
+++ movieApp/src/components/Card.jsx
@@ -1,6 +1,23 @@
+import backup from '../assets/backup.jpg'
+import { Link } from 'react-router-dom'
 export const Card = () => {
   return (
-    <div>Card</div>
+    <div className="col shadow-sm">
+      <div className="card shadow">
+                <img className='card-img-top ' src={backup} alt="" />
+      <div className="card-body">
+        <h5 className='card-title text-primary'>Movie Title</h5>
+        <p className='card-text'>curated collections, and quick searches – your guide to finding</p>
+        <div className='d-flex justify-content-between align-item-center'>
+          <Link to="" className="btn btn-sm btn-outline-primary">
+          Read more</Link>
+          <small>
+            <i className='bi bi-star-fill text-warning'></i>
+            7.25 | 250 Review
+          </small>
+        </div>
+      </div>
+      </div>
+    </div>
   )
 }
-
--- movieApp/src/pages/MovieList.jsx
+++ movieApp/src/pages/MovieList.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import { Card } from '../components';
 
 export const MovieList = ({ title }) => {
   useEffect(() => {
@@ -16,13 +17,21 @@ export const MovieList = ({ title }) => {
               Discover movies you'll love with personalized suggestions, curated collections, and quick searches – your guide to finding great films
             </p>
             <button onClick={()=>{navigator("/movies/upcoming")}} className='btn btn-primary'>Explore Now</button>
-            
+
           </div>
         ) : (
-          null
+          ""
         )}
 
         <h5 className='text-danger py-2  border-bottom'>{title}</h5>
+        <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 py-2 '>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
       </main>
     </div>
   )
